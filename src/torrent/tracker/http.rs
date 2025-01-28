a7dfa89afd6d86552dc4cfb8611bb0e6918385b5
Alex Ripoll <alexripollpiera@gmail.com>
2025-01-28 17:56:16 +0100

    create TrackerResponse for http mod + move logic

--- src/torrent/tracker/http.rs
+++ src/torrent/tracker/http.rs
@@ -1,9 +1,13 @@
+use serde_bencode::de;
+use std::collections::HashMap;
+use std::net::Ipv4Addr;
+
 use percent_encoding::{percent_encode, NON_ALPHANUMERIC};
 use url::Url;
 
-use crate::torrent::peer::Peer;
+use crate::torrent::peer::{Ip, Peer};
 
-use super::tracker::{Announce, TrackerError, TrackerProtocol, TrackerResponse};
+use super::tracker::{Announce, TrackerError, TrackerProtocol};
 
 pub struct HttpTracker {}
 
@@ -70,12 +74,126 @@ impl TrackerProtocol for HttpTracker {
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
+pub enum TrackerResponse {
+    Success {
+        tracker_id: Option<String>,
+        interval: u32,
+        min_interval: Option<u32>,
+        seeders: u32,
+        leechers: u32,
+        peers: Vec<u8>,
+        warning: Option<String>,
+    },
+    Failure {
+        message: String,
+    },
+}
+
+impl TrackerResponse {
+    pub fn from_bencoded(response: &[u8]) -> Result<TrackerResponse, TrackerError> {
+        // Deserialize the bencoded data into a temporary intermediate structure
+        let raw: HashMap<String, serde_bencode::value::Value> = match de::from_bytes(response) {
+            Ok(val) => val,
+            Err(err) => return Err(TrackerError::ParseError(err)),
+        };
+
+        // Check for failure response
+        if let Some(serde_bencode::value::Value::Bytes(msg)) = raw.get("failure_reason") {
+            return Ok(TrackerResponse::Failure {
+                message: String::from_utf8_lossy(msg).to_string(),
+            });
+        }
+
+        // Extract fields for a successful response
+        let interval = match raw.get("interval") {
+            Some(serde_bencode::value::Value::Int(i)) if *i >= 0 => *i as u32,
+            _ => return Err(TrackerError::ResponseMissingField("interval".to_string())),
+        };
+
+        let min_interval = raw.get("min interval").and_then(|v| match v {
+            serde_bencode::value::Value::Int(i) if *i >= 0 => Some(*i as u32),
+            _ => None,
+        });
+
+        let tracker_id = raw.get("tracker_id").and_then(|v| match v {
+            serde_bencode::value::Value::Bytes(b) => Some(String::from_utf8_lossy(b).to_string()),
+            _ => None,
+        });
+
+        let seeders = match raw.get("complete") {
+            Some(serde_bencode::value::Value::Int(i)) if *i >= 0 => *i as u32,
+            _ => 0,
+        };
+
+        let leechers = match raw.get("incomplete") {
+            Some(serde_bencode::value::Value::Int(i)) if *i >= 0 => *i as u32,
+            _ => 0,
+        };
+
+        let warning = raw.get("warning_message").and_then(|v| match v {
+            serde_bencode::value::Value::Bytes(b) => Some(String::from_utf8_lossy(b).to_string()),
+            _ => None,
+        });
+
+        let peers = match raw.get("peers") {
+            Some(serde_bencode::value::Value::Bytes(bytes)) => bytes.clone(),
+            _ => vec![], // No peers found
+        };
+
+        // Build the Ok variant
+        Ok(TrackerResponse::Success {
+            tracker_id,
+            interval,
+            min_interval,
+            seeders,
+            leechers,
+            peers,
+            warning,
+        })
+    }
+
+    pub fn decode_peers(&self) -> Result<Vec<Peer>, TrackerError> {
+        match &self {
+            TrackerResponse::Success { peers, .. } => {
+                if !peers.is_empty() {
+                    if peers.len() % 6 != 0 {
+                        return Err(TrackerError::InvalidPeersFormat);
+                    }
+                    let peers: Vec<Peer> = peers
+                        .chunks(6)
+                        .map(|peer_bytes| Peer {
+                            peer_id: None,
+                            ip: Ip::IpV4(Ipv4Addr::new(
+                                peer_bytes[0],
+                                peer_bytes[1],
+                                peer_bytes[2],
+                                peer_bytes[3],
+                            )),
+                            port: u16::from_be_bytes([peer_bytes[4], peer_bytes[5]]),
+                        })
+                        .collect();
+                    return Ok(peers);
+                }
+            }
+            TrackerResponse::Failure { .. } => {}
+        }
+
+        Err(TrackerError::EmptyPeers)
+    }
+}
+
 #[cfg(test)]
 mod test {
 
-    use crate::torrent::tracker::{
-        http::HttpTracker,
-        tracker::{Announce, Event},
+    use std::net::Ipv4Addr;
+
+    use crate::torrent::{
+        peer::{Ip, Peer},
+        tracker::{
+            http::{HttpTracker, TrackerResponse},
+            tracker::{Announce, Event, TrackerError},
+        },
     };
 
     #[test]
@@ -115,5 +233,103 @@ mod test {
             "https://torrent.ubuntu.com/announce?info_hash=%124Vx%9A%BC%DE%F1%23Eg%89%AB%CD%EF%124Vx%9A&peer_id=%2DJM0100%2DXPGcHeKEmI45&port=6889&uploaded=0&downloaded=0&left=5665497088&compact=1",
             url.as_str()
         );
+    }
+    #[test]
+    fn test_tracker_response_from_bencoded_success_variant() {
+        let bencoded_body =
+            b"d8:completei990e10:incompletei63e8:intervali1800e10:tracker_id10:tracker12315:warning_message26:This is a warning message!5:peers6:\xb9}\xbe;\x1b\x14e";
+
+        let response = TrackerResponse::from_bencoded(bencoded_body).unwrap();
+        let expected_response = TrackerResponse::Success {
+            interval: 1800,
+            min_interval: None,
+            tracker_id: Some(String::from("tracker123")),
+            // tracker_id: None,
+            seeders: 990,
+            leechers: 63,
+            peers: vec![185, 125, 190, 59, 27, 20].into(),
+            warning: Some(String::from("This is a warning message!")),
+        };
+
+        assert_eq!(expected_response, response);
+    }
+
+    #[test]
+    fn test_tracker_response_from_bencoded_failure_variant() {
+        let bencoded_body = b"d14:failure_reason28:Error: Something went wrong!e";
+
+        let response = TrackerResponse::from_bencoded(bencoded_body).unwrap();
+        let expected_response = TrackerResponse::Failure {
+            message: "Error: Something went wrong!".to_string(),
+        };
+
+        assert_eq!(expected_response, response);
+    }
+
+    #[test]
+    fn test_peers_binary_model_decode() {
+        let response = TrackerResponse::Success {
+            tracker_id: None,
+            interval: 100,
+            min_interval: None,
+            seeders: 20,
+            leechers: 5,
+            peers: vec![185, 125, 190, 59, 26, 247, 187, 125, 192, 48, 26, 233],
+            warning: None,
+        };
+
+        let expected = vec![
+            Peer {
+                peer_id: None,
+                ip: Ip::IpV4(Ipv4Addr::new(185, 125, 190, 59)),
+                port: 6903,
+            },
+            Peer {
+                peer_id: None,
+                ip: Ip::IpV4(Ipv4Addr::new(187, 125, 192, 48)),
+                port: 6889,
+            },
+        ];
+
+        assert_eq!(expected, response.decode_peers().unwrap());
+    }
+
+    #[test]
+    fn test_peers_binary_model_decode_error_invalid_format() {
+        let response = TrackerResponse::Success {
+            tracker_id: None,
+            interval: 100,
+            min_interval: None,
+            seeders: 20,
+            leechers: 5,
+            peers: vec![185, 125, 190, 59, 26, 247, 187, 125, 192, 48],
+            warning: None,
+        };
+
+        assert!(matches!(
+            response
+                .decode_peers()
+                .expect_err("Expected InvalidPeersFormat error"),
+            TrackerError::InvalidPeersFormat,
+        ));
+    }
+    #[test]
+    fn test_peers_binary_model_decode_error_empty_peers() {
+        let response = TrackerResponse::Success {
+            tracker_id: None,
+            interval: 100,
+            min_interval: None,
+            seeders: 20,
+            leechers: 5,
+            peers: vec![],
+            warning: None,
+        };
+
+        assert!(matches!(
+            response
+                .decode_peers()
+                .expect_err("Expected EmptyPeers error"),
+            TrackerError::EmptyPeers,
+        ));
     }
 }
--- src/torrent/tracker/tracker.rs
+++ src/torrent/tracker/tracker.rs
@@ -1,18 +1,19 @@
-use core::panic;
 use serde::{Deserialize, Serialize};
-use serde_bencode::de;
 use serde_bytes::ByteBuf;
 use std::{
     collections::{HashMap, VecDeque},
     error::Error,
     fmt::Display,
-    net::{IpAddr, Ipv4Addr},
+    net::IpAddr,
 };
 use url::Url;
 
-use crate::torrent::peer::{Ip, Peer};
+use crate::torrent::peer::Peer;
 
-use super::{http::HttpTracker, udp::UdpTracker};
+use super::{
+    http::{self, HttpTracker},
+    udp::UdpTracker,
+};
 
 pub struct Tracker {
     port: u16,
@@ -70,112 +71,14 @@ pub struct Flags {
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum TrackerResponse {
-    Success {
-        tracker_id: Option<String>,
-        interval: u32,
-        min_interval: Option<u32>,
-        seeders: u32,
-        leechers: u32,
-        peers: Vec<u8>,
-        warning: Option<String>,
-    },
-    Failure {
-        message: String,
-    },
+    Http(http::TrackerResponse),
 }
 
-impl TrackerResponse {
-    pub fn from_bencoded(response: &[u8]) -> Result<TrackerResponse, TrackerError> {
-        // Deserialize the bencoded data into a temporary intermediate structure
-        let raw: HashMap<String, serde_bencode::value::Value> = match de::from_bytes(response) {
-            Ok(val) => val,
-            Err(err) => return Err(TrackerError::ParseError(err)),
-        };
-
-        // Check for failure response
-        if let Some(serde_bencode::value::Value::Bytes(msg)) = raw.get("failure_reason") {
-            return Ok(TrackerResponse::Failure {
-                message: String::from_utf8_lossy(msg).to_string(),
-            });
-        }
-
-        // Extract fields for a successful response
-        let interval = match raw.get("interval") {
-            Some(serde_bencode::value::Value::Int(i)) if *i >= 0 => *i as u32,
-            _ => return Err(TrackerError::ResponseMissingField("interval".to_string())),
-        };
-
-        let min_interval = raw.get("min interval").and_then(|v| match v {
-            serde_bencode::value::Value::Int(i) if *i >= 0 => Some(*i as u32),
-            _ => None,
-        });
-
-        let tracker_id = raw.get("tracker_id").and_then(|v| match v {
-            serde_bencode::value::Value::Bytes(b) => Some(String::from_utf8_lossy(b).to_string()),
-            _ => None,
-        });
-
-        let seeders = match raw.get("complete") {
-            Some(serde_bencode::value::Value::Int(i)) if *i >= 0 => *i as u32,
-            _ => 0,
-        };
-
-        let leechers = match raw.get("incomplete") {
-            Some(serde_bencode::value::Value::Int(i)) if *i >= 0 => *i as u32,
-            _ => 0,
-        };
-
-        let warning = raw.get("warning_message").and_then(|v| match v {
-            serde_bencode::value::Value::Bytes(b) => Some(String::from_utf8_lossy(b).to_string()),
-            _ => None,
-        });
-
-        let peers = match raw.get("peers") {
-            Some(serde_bencode::value::Value::Bytes(bytes)) => bytes.clone(),
-            _ => vec![], // No peers found
-        };
-
-        // Build the Ok variant
-        Ok(TrackerResponse::Success {
-            tracker_id,
-            interval,
-            min_interval,
-            seeders,
-            leechers,
-            peers,
-            warning,
-        })
-    }
-
-    pub fn decode_peers(&self) -> Result<Vec<Peer>, TrackerError> {
-        match &self {
-            TrackerResponse::Success { peers, .. } => {
-                if !peers.is_empty() {
-                    if peers.len() % 6 != 0 {
-                        return Err(TrackerError::InvalidPeersFormat);
-                    }
-                    let peers: Vec<Peer> = peers
-                        .chunks(6)
-                        .map(|peer_bytes| Peer {
-                            peer_id: None,
-                            ip: Ip::IpV4(Ipv4Addr::new(
-                                peer_bytes[0],
-                                peer_bytes[1],
-                                peer_bytes[2],
-                                peer_bytes[3],
-                            )),
-                            port: u16::from_be_bytes([peer_bytes[4], peer_bytes[5]]),
-                        })
-                        .collect();
-                    return Ok(peers);
-                }
-            }
-            TrackerResponse::Failure { .. } => {}
-        }
-
-        Err(TrackerError::EmptyPeers)
-    }
-}
+// enum Udp {
+//     Connect(response),
+//     Announce(response),
+//     Error(response),
+// }
 
 pub trait TrackerProtocol {
     async fn get_peers(
@@ -214,59 +117,6 @@ impl Tracker {
             "udp" => self.udp.get_peers(&announce_url, announce_data).await,
             _ => Err(TrackerError::UnsupportedProtocol(url.scheme().to_string())),
         }
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
-pub struct Response {
-    pub failure_response: Option<String>,
-    warning_message: Option<String>,
-    pub interval: Option<u32>,
-    min_interval: Option<u32>,
-    tracker_id: Option<String>,
-    pub complete: Option<u32>,   // number of seeders
-    pub incomplete: Option<u32>, // number of leechers
-    // TODO: deocde both Peers variants
-    // peers: Option<Peers>,
-    pub peers: Option<ByteBuf>,
-}
-
-impl Response {
-    pub fn empty() -> Self {
-        Response {
-            failure_response: None,
-            warning_message: None,
-            interval: None,
-            min_interval: None,
-            tracker_id: None,
-            complete: None,
-            incomplete: None,
-            peers: None,
-        }
-    }
-
-    pub fn decode_peers(&self) -> Result<Vec<Peer>, TrackerError> {
-        if let Some(peers_bytes) = &self.peers {
-            if peers_bytes.len() % 6 != 0 {
-                return Err(TrackerError::InvalidPeersFormat);
-            }
-            let peers: Vec<Peer> = peers_bytes
-                .chunks(6)
-                .map(|peer_bytes| Peer {
-                    peer_id: None,
-                    ip: Ip::IpV4(Ipv4Addr::new(
-                        peer_bytes[0],
-                        peer_bytes[1],
-                        peer_bytes[2],
-                        peer_bytes[3],
-                    )),
-                    port: u16::from_be_bytes([peer_bytes[4], peer_bytes[5]]),
-                })
-                .collect();
-            return Ok(peers);
-        }
-
-        Err(TrackerError::EmptyPeers)
     }
 }
 
@@ -324,113 +174,5 @@ impl Error for TrackerError {
             TrackerError::ParseError(err) => Some(err),
             _ => None,
         }
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use std::net::Ipv4Addr;
-
-    use crate::torrent::tracker::tracker::TrackerError;
-
-    use super::{Ip, Peer, TrackerResponse};
-
-    #[test]
-    fn test_tracker_response_from_bencoded_success_variant() {
-        let bencoded_body =
-            b"d8:completei990e10:incompletei63e8:intervali1800e10:tracker_id10:tracker12315:warning_message26:This is a warning message!5:peers6:\xb9}\xbe;\x1b\x14e";
-
-        let response = TrackerResponse::from_bencoded(bencoded_body).unwrap();
-        let expected_response = TrackerResponse::Success {
-            interval: 1800,
-            min_interval: None,
-            tracker_id: Some(String::from("tracker123")),
-            // tracker_id: None,
-            seeders: 990,
-            leechers: 63,
-            peers: vec![185, 125, 190, 59, 27, 20].into(),
-            warning: Some(String::from("This is a warning message!")),
-        };
-
-        assert_eq!(expected_response, response);
-    }
-
-    #[test]
-    fn test_tracker_response_from_bencoded_failure_variant() {
-        let bencoded_body = b"d14:failure_reason28:Error: Something went wrong!e";
-
-        let response = TrackerResponse::from_bencoded(bencoded_body).unwrap();
-        let expected_response = TrackerResponse::Failure {
-            message: "Error: Something went wrong!".to_string(),
-        };
-
-        assert_eq!(expected_response, response);
-    }
-
-    #[test]
-    fn test_peers_binary_model_decode() {
-        let response = TrackerResponse::Success {
-            tracker_id: None,
-            interval: 100,
-            min_interval: None,
-            seeders: 20,
-            leechers: 5,
-            peers: vec![185, 125, 190, 59, 26, 247, 187, 125, 192, 48, 26, 233],
-            warning: None,
-        };
-
-        let expected = vec![
-            Peer {
-                peer_id: None,
-                ip: Ip::IpV4(Ipv4Addr::new(185, 125, 190, 59)),
-                port: 6903,
-            },
-            Peer {
-                peer_id: None,
-                ip: Ip::IpV4(Ipv4Addr::new(187, 125, 192, 48)),
-                port: 6889,
-            },
-        ];
-
-        assert_eq!(expected, response.decode_peers().unwrap());
-    }
-
-    #[test]
-    fn test_peers_binary_model_decode_error_invalid_format() {
-        let response = TrackerResponse::Success {
-            tracker_id: None,
-            interval: 100,
-            min_interval: None,
-            seeders: 20,
-            leechers: 5,
-            peers: vec![185, 125, 190, 59, 26, 247, 187, 125, 192, 48],
-            warning: None,
-        };
-
-        assert!(matches!(
-            response
-                .decode_peers()
-                .expect_err("Expected InvalidPeersFormat error"),
-            TrackerError::InvalidPeersFormat,
-        ));
-    }
-    #[test]
-    fn test_peers_binary_model_decode_error_empty_peers() {
-        let response = TrackerResponse::Success {
-            tracker_id: None,
-            interval: 100,
-            min_interval: None,
-            seeders: 20,
-            leechers: 5,
-            peers: vec![],
-            warning: None,
-        };
-
-        assert!(matches!(
-            response
-                .decode_peers()
-                .expect_err("Expected EmptyPeers error"),
-            TrackerError::EmptyPeers,
-        ));
     }
 }
