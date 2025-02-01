532fba589e3ce9ce5009d115e2b1ec323e9fec98
Alex Ripoll <alexripollpiera@gmail.com>
2025-02-01 16:04:33 +0100

    torrent download state modulue

---
+++ src/torrent/state.rs
@@ -0,0 +1,117 @@
+use std::collections::{HashMap, HashSet};
+
+use protocol::bitfield::Bitfield;
+use protocol::piece::Piece;
+
+#[derive(Debug)]
+pub struct State {
+    bitfield: Bitfield,                      // Bitfield of the downloaded pieces
+    peers_bitfield: Vec<(String, Bitfield)>, // Peers' bitfield from all peer connections
+    pieces_map: HashMap<u32, Piece>,         // Piece index - Piece struct Map
+    pieces_rarity: Vec<u32>, // Containing the amount of occurences of a piece index within
+    // the  current peers' bitfieds
+    pending_pieces: Vec<u32>, // Sorted set of pieces pending to be downloaded (rarest first)
+    assigned_pieces: HashSet<u32>, // Indexes of the pieces being downloaded
+}
+
+impl State {
+    pub async fn process_bitfield(&mut self, peer_bitfield: &Bitfield) {
+        let missing_pieces_bitfield = self.build_wanted_pieces_bitfield(peer_bitfield);
+
+        self.increase_pieces_rarity(&missing_pieces_bitfield);
+        self.pending_pieces = self.sort_pieces();
+    }
+
+    fn build_wanted_pieces_bitfield(&self, peer_bitfield: &Bitfield) -> Bitfield {
+        let mut missing = Vec::new();
+
+        for (peer_byte, state_byte) in peer_bitfield.bytes.iter().zip(self.bitfield.bytes.iter()) {
+            missing.push(peer_byte & !state_byte);
+        }
+
+        Bitfield::new(&missing)
+    }
+
+    pub fn downloaded_pieces_count(&self) -> u32 {
+        self.bitfield
+            .bytes
+            .iter()
+            .map(|byte| byte.count_ones())
+            .sum::<u32>()
+    }
+
+    pub fn unassign_pieces(&mut self, pieces_index: Vec<u32>) {
+        for piece_index in pieces_index {
+            self.unassign_piece(piece_index);
+        }
+    }
+
+    // Mark a piece as downloaded, remove from missing and assigned sets
+    fn mark_piece_complete(&mut self, piece_index: u32) {
+        self.bitfield.set_piece(piece_index as usize);
+        self.pending_pieces.remove(piece_index as usize);
+        self.assigned_pieces.remove(&piece_index);
+        // set to 0 so it is not taken in consideration since it is already set in the `bitfield` field.
+        self.pieces_rarity[piece_index as usize] = 0;
+    }
+
+    pub async fn increase_pieces_rarity(&mut self, peer_bitfield: &Bitfield) {
+        for byte_index in 0..peer_bitfield.bytes.len() {
+            for bit_index in 0..8 {
+                let piece_index = (byte_index * 8 + bit_index) as u32;
+                if peer_bitfield.has_piece(piece_index as usize) {
+                    self.pieces_rarity[piece_index as usize] += 1;
+                }
+            }
+        }
+    }
+
+    pub fn sort_pieces(&self) -> Vec<u32> {
+        // remove already downloaded or unavailable pieces
+        let mut indices: Vec<u32> = self
+            .pieces_rarity
+            .iter()
+            .enumerate()
+            .filter(|&(_, &count)| count > 0)
+            .map(|(piece_index, _)| piece_index as u32)
+            .collect();
+
+        // sort pieces by rarity (rarest first)
+        indices.sort_by(|&a, &b| {
+            let count_a = self.pieces_rarity[a as usize];
+            let count_b = self.pieces_rarity[b as usize];
+            count_a.cmp(&count_b).then_with(|| a.cmp(&b))
+        });
+
+        indices
+    }
+
+    pub fn populate_queue(&mut self, sorted_pieces: Vec<u32>) {
+        self.pending_pieces.clear();
+        self.pending_pieces.extend(sorted_pieces);
+    }
+
+    pub fn assign_piece(&mut self, peer_bitfield: &Bitfield) -> Option<u32> {
+        if let Some(pos) = self.pending_pieces.iter().position(|&piece_index| {
+            !self.assigned_pieces.contains(&piece_index)
+                && peer_bitfield.has_piece(piece_index as usize)
+        }) {
+            let piece_index = self.pending_pieces.remove(pos);
+            self.assigned_pieces.insert(piece_index.clone());
+            return Some(piece_index);
+        }
+
+        None
+    }
+
+    pub fn unassign_piece(&mut self, piece_index: u32) {
+        self.assigned_pieces.remove(&piece_index);
+        self.pending_pieces.push(piece_index);
+    }
+
+    pub fn download_progress_percent(&self) -> u32 {
+        let downloaded_pieces = self.downloaded_pieces_count();
+
+        downloaded_pieces * 100 / self.bitfield.total_pieces as u32
+    }
+}
